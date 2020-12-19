acec7c991a98b08ae3cad1b5e5f3cdfc3cc1dfb9
Aaron Ebergard <yakunin901@gmail.com>
2020-12-19 20:13:30 +0300

    2013 19

---
+++ p0291simpleactivityresult/src/main/java/com/startandoid/p0291simpleactivityresult/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.startandoid.p0291simpleactivityresult
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
